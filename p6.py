4d428b1121587ccdd12e5defc73f0928fcfb08e4
vasanthprasath <vasanthprasathsekar@gmail.com>
2025-08-16 19:47:49 +0530

    Add files via upload

---
+++ p6.py
@@ -0,0 +1,7 @@
+from collections import Counter
+
+my_string = "programming"
+char_counts = Counter(my_string)
+print(char_counts)
+#print(f"The character 'g' appears {char_counts['g']} times.")
+if char_counts
